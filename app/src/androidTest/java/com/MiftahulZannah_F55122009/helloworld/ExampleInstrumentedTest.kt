ad7d58eeff1d720cca549aac0f43c1504b13acff
irmasttrh <irmasittirahma2@gmail.com>
2023-09-23 17:14:37 +0700

    Initial commit

---
+++ app/src/androidTest/java/com/MiftahulZannah_F55122009/helloworld/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.MiftahulZannah_F55122009.helloworld
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.MiftahulZannah_F55122009.helloworld", appContext.packageName)
+    }
+}
